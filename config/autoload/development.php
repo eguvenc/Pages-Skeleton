204d4781f5478da26f58f6b3af843351a78d28bd
Ersin Guvenc <eguvenc@gmail.com>
2020-06-20 12:23:14 +0300

    Create development.php

---
+++ config/autoload/development.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Local Configuration Override for development environment.
+ *
+ * This configuration override file is for providing configuration to use while
+ * in development environment.
+ */
+
+return [
+    'view_manager' => [
+        'display_exceptions' => true,
+    ],
+
+    'service_manager' => [
+        'initializers' => [
+
+        ],
+    ]
+];
